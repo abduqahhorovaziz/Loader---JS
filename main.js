8a6aa1a13d0c0760a63656aa295f36bf5ec7591f
abduqahhorovaziz <83882515+abduqahhorovaziz@users.noreply.github.com>
2021-11-23 11:36:25 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const loader = document.querySelector('.loader')
+    setTimeout(() => {
+        loader.style.opacity = '0'
+        setTimeout(()=>{
+            loader.style.display = 'none'
+        }, 500)
+    }, 2000)
+})
+
+
+
+
+
+
+
+
+
+// setTimeout(function(){
+//     alert("Hi")
+// }, 1000)
+
+// setInterval(function(){
+//     alert("Hello")
+// }, 2000)
